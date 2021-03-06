MRtecno98/FactorioModPortal

commit 6e038129b1e3087e933fe93bb19c51e2a33c70fa

Fixed #1

factoriomod.py
@@ -159,13 +159,13 @@ def downloadMod(packet) :
 	release = None
 	for rl in  packet["releases"] :
 		if rl["version"] == vers :
-				release = rl
-	
-	url = "http://mods.factorio.com" + rl["download_url"] + "?username=" + username + "&token=" + token
+				release = dict(rl)
+		
+	url = "http://mods.factorio.com" + release["download_url"] + "?username=" + username + "&token=" + token
 	
 	request = requests.get(url)
 	request.raise_for_status()
-	with open("mod_cache" + os.sep + rl["file_name"], "wb") as file:
+	with open("mod_cache" + os.sep + release["file_name"], "wb") as file:
 		for chunk in request.iter_content(4096) :
 			file.write(chunk)
 

commit 214d5b7e16b16458d4348d69075184c5549e1d88

Fixed bug introduced by 6e03812

factoriomod.py
@@ -170,7 +170,7 @@ def downloadMod(packet) :
 			file.write(chunk)
 
 	print("Mod downloaded successfully")
-	return rl["file_name"]
+	return release["file_name"]
 
 def installMod(filename) :
 	global factorio_path